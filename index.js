2409862aa9844e9de87b4d87560d331033ab1efc
fahmij8 <fahmijabbar12@gmail.com>
2022-12-12 19:49:10 +0700

    feat: initialize express app

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.listen(process.env.PORT || 3001);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/api', (req, res) => {
+  res.send({
+    message: 'Hello World!',
+  });
+});
