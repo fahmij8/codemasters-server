d5ed16a3a9835a61aef10762c8610ec3800888a2
fahmij8 <fahmijabbar12@gmail.com>
2022-12-12 20:14:06 +0700

    feat: integrate responses

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
-app.listen(process.env.PORT || 3001);
+const port = process.env.PORT || 3001;
+const responses = require('./src/responses');
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.redirect('/api');
 });
 
 app.get('/api', (req, res) => {
   res.send({
-    message: 'Hello World!',
+    message: 'Hello World API Codemasters!',
+    enpoint: ['/api/lessons', '/api/lessons/sample'],
   });
 });
+
+app.get('/api/lessons/sample', (req, res) => {
+  res.send(responses.SAMPLE_LESSON_DATA);
+});
+
+app.listen(port, () => {
+  console.log(`Server Started at ${port}`);
+});
+
+module.exports = app;
